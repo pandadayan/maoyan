4532bf89905996f154df0aa7f9aa43429ac01b42
Pan Yan <443685507@qq.com>
2019-11-08 20:46:31 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <Layout class=""/>
+  </div>
+</template>
+
+<script>
+import Layout from './layout'
+
+import './assets/css/global.styl'
+import './utils/res'
+
+export default {
+  name: 'app',
+  components: {
+    Layout
+  }
+}
+</script>
+
+<style lang="stylus">
+
+</style>
